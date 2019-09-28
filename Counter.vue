a3cd3a848c07b9c3a5b051659ce6f85f565b17f8
thomasgudjonwright <50252341+thomasgudjonwright@users.noreply.github.com>
2019-09-27 20:40:23 -0400

    Create Counter.vue

---
+++ Counter.vue
@@ -0,0 +1,30 @@
+<template>
+<div>
+  <p>{{count}}</p>
+  <button v-on:click="inc">Add 1</button>
+  <button v-on:click="dec">Subtract 1</button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      count: episodes_seen
+    }
+  },
+  methods: {
+    inc: function() {
+      this.count++;
+    },
+    dec: function() {
+      this.count--;
+    }
+  }
+};
+</script>
+
+<style>
+</style>
+
