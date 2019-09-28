93f736a7f523649c7dc9656601fea85889eaacf1
thomasgudjonwright <50252341+thomasgudjonwright@users.noreply.github.com>
2019-09-27 20:40:57 -0400

    Create Show.vue

---
+++ Show.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+  <p>{{name}}</p>
+   <Counter></Counter>
+  </div>
+</template>
+
+<script>
+  import Counter from "./Counter.vue";
+export default {
+  components: {
+    Counter,
+  },
+  data(){
+    return{
+      props: {
+        name: String,
+        id: Number,
+        episodes_seen: Number
+      }
+    }
+  }
+};
+</script>
+
+<style>
+</style>
